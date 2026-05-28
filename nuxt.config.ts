e003c2b37361e28e81a17f5f5c5e353db692cab8
Therealkosbruh <realkoslulz@gmail.com>
2026-05-28 18:55:54 +0300

    feat: linter, prettier, pre-commit stage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,61 +1,75 @@
-import Aura from "@primeuix/themes/aura";
+import Aura from '@primeuix/themes/aura'
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
-    "@primevue/nuxt-module",
-    "@nuxtjs/color-mode",
-    "@pinia/nuxt",
-    "@nuxtjs/i18n",
-    "@nuxt/image",
+    '@primevue/nuxt-module',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxt/image',
+    '@nuxt/eslint',
   ],
   primevue: {
     options: {
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: ".dark",
+          darkModeSelector: '.dark',
           cssLayer: false,
         },
       },
     },
   },
   colorMode: {
-    classSuffix: "",
+    classSuffix: '',
   },
   i18n: {
     locales: [
       {
-        code: "en",
-        language: "en-US",
-        files: ["en/common.json", "en/home.json", "en/story.json", "en/bookmarks.json"],
+        code: 'en',
+        language: 'en-US',
+        files: ['en/common.json', 'en/home.json', 'en/story.json', 'en/bookmarks.json'],
       },
       {
-        code: "ru",
-        language: "ru-RU",
-        files: ["ru/common.json", "ru/home.json", "ru/story.json", "ru/bookmarks.json"],
+        code: 'ru',
+        language: 'ru-RU',
+        files: ['ru/common.json', 'ru/home.json', 'ru/story.json', 'ru/bookmarks.json'],
       },
     ],
-    defaultLocale: "en",
-    langDir: "locales",
+    defaultLocale: 'en',
+    langDir: 'locales',
     compilation: {
       strictMessage: false,
     },
   },
   imports: {
-    dirs: [
-      "src/entities/*/model",
-      "src/features/*/model",
-      "src/shared/composables",
-    ],
+    dirs: ['src/entities/*/model', 'src/features/*/model', 'src/shared/composables'],
   },
   components: [
-    { path: "~/src/shared/ui", pathPrefix: false },
-    { path: "~/src/entities/story/ui", pathPrefix: false },
-    { path: "~/src/entities/comment/ui", pathPrefix: false },
-    { path: "~/src/features/filters/ui", pathPrefix: false },
-    { path: "~/src/widgets", pathPrefix: false },
+    { path: '~/src/shared/ui', pathPrefix: false },
+    { path: '~/src/entities/story/ui', pathPrefix: false },
+    { path: '~/src/entities/comment/ui', pathPrefix: false },
+    { path: '~/src/features/filters/ui', pathPrefix: false },
+    { path: '~/src/widgets', pathPrefix: false },
   ],
-  css: ["~/src/app/styles/global.scss"],
-});
+  css: ['~/src/app/styles/global.scss'],
+
+  nitro: {
+    compressPublicAssets: true,
+  },
+
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vue-vendor': ['vue', 'vue-router', 'pinia'],
+            'ui-vendor': ['primevue'],
+          },
+        },
+      },
+    },
+  },
+})
